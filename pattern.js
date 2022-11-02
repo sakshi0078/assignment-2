d19b2aed081a3875e5163265940560fb4a1363bf
RounakDhatterwal <rounakdhatterwal43215@gmail.com>
2022-11-02 13:24:55 +0530

    corrected the code

--- pattern.js
+++ pattern.js
@@ -4,27 +4,31 @@
 
 // If the value stored inN = 5, then the pattern required will be
 
-// * * * * *
 // *       *
+//   *   *  
+//     *    
+//   *   *  
 // *       *
-// *       *
-// * * * * *
+
+
 let N=5;
 for(let i=1; i<=N; i++){
+
   let bag="";
-  for(let j=1; j<=N; j++){
-    if(i==1 || i==N){
-      bag+="* "
+  
+for(let j=1; j<=N; j++){
+
+    if( i == j || i + j == (N+1)){
+
+      bag+="* ";
+
     }
-    else{
-      if(j==1 || j==N){
-        bag+="* "
-      }
      
       else{
-        bag+="  "
+       
+ bag+="  " ;
       }
     }
-  }
-  console.log(bag)
-}
+  console.log(bag);
+ }
+ 
